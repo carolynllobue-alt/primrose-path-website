2cb8145435d1216645bb84e33f285296784bd9f6
carolynllobue-alt <carolynllobue@gmail.com>
2026-05-19 16:15:55 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,14 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 
-createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <>
+      <App />
+      <Analytics />
+    </>
+  </React.StrictMode>
+);
